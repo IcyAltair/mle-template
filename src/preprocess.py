30c4b327127077f1073ac96de5af9c183a572026
Kitya <kittitereh@kittitereh.(none)>
2022-12-12 21:15:31 +0300

    finish changes in preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,6 @@
 import configparser
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import sys
 import traceback
 
@@ -16,52 +15,69 @@ class DataMaker():
         logger = Logger(SHOW_LOG)
         self.config = configparser.ConfigParser()
         self.log = logger.get_logger(__name__)
-        self.project_path = os.path.join(os.getcwd(), "data")
+        self.project_path = os.path.join(os.getcwd(), "../data")
         self.train_path = os.path.join(self.project_path, "fashion-mnist_train.csv")
         self.test_path = os.path.join(self.project_path, "fashion-mnist_test.csv")
+        self.X_train_path = os.path.join(self.project_path, "fashion_X_train.csv")
+        self.y_train_path = os.path.join(self.project_path, "fashion_y_train.csv")
+        self.X_test_path = os.path.join(self.project_path, "fashion_X_test.csv")
+        self.y_test_path = os.path.join(self.project_path, "fashion_y_test.csv")
         self.data_path = [os.path.join(self.train_path, 'fashion-mnist_train.csv'),
                           os.path.join(self.test_path, 'fashion-mnist_test.csv')]
         self.log.info("DataMaker is ready")
 
-    def get_data(self) -> bool:
-        dataset = pd.read_csv(self.data_path)
-        X = pd.DataFrame(dataset.iloc[:, 1:5].values)
-        y = pd.DataFrame(dataset.iloc[:, 5:].values)
-        X.to_csv(self.X_path, index=True)
-        y.to_csv(self.y_path, index=True)
-        if os.path.isfile(self.X_path) and os.path.isfile(self.y_path):
-            self.log.info("X and y data is ready")
-            self.config["DATA"] = {'X_data': self.X_path,
-                                   'y_data': self.y_path}
-            return os.path.isfile(self.X_path) and os.path.isfile(self.y_path)
+    def get_data(self) -> tuple:
+
+        test = pd.read_csv(self.test_path)
+        train = pd.read_csv(self.train_path)
+
+        if os.path.isfile(self.test_path) and os.path.isfile(self.train_path):
+            self.log.info("data is ready")
+            self.config["DATA"] = {'train_data': self.train_path,
+                                   'test_data': self.test_path}
+            return test, train
         else:
-            self.log.error("X and y data is not ready")
-            return False
+            self.log.error("train and test data is not ready")
+            return ()
 
     def split_data(self):
+
         try:
-            X = pd.read_csv(self.X_path, index_col=0)
-            y = pd.read_csv(self.y_path, index_col=0)
+            test, train = self.get_data()
+
         except FileNotFoundError:
             self.log.error(traceback.format_exc())
             sys.exit(1)
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=test_size, random_state=0)
-        self.save_splitted_data(X_train, self.train_path[0])
-        self.save_splitted_data(y_train, self.train_path[1])
-        self.save_splitted_data(X_test, self.test_path[0])
-        self.save_splitted_data(y_test, self.test_path[1])
-        self.config["SPLIT_DATA"] = {'X_train': self.train_path[0],
-                                     'y_train': self.train_path[1],
-                                     'X_test': self.test_path[0],
-                                     'y_test': self.test_path[1]}
-        self.log.info("Train and test data is ready")
+
+
+        X_train = pd.DataFrame(train.iloc[:, 1:].values)
+        y_train = pd.DataFrame(train.iloc[:, 0].values)
+        X_test = pd.DataFrame(test.iloc[:, 1:].values)
+        y_test = pd.DataFrame(test.iloc[:, 0].values)
+
+        X_train.to_csv(self.X_train_path, index=True)
+        y_train.to_csv(self.y_train_path, index=True)
+        X_test.to_csv(self.X_test_path, index=True)
+        y_test.to_csv(self.y_test_path, index=True)
+
+
+        self.save_splitted_data(X_train, self.X_train_path)
+        self.save_splitted_data(y_train, self.y_train_path)
+        self.save_splitted_data(X_test, self.X_test_path)
+        self.save_splitted_data(y_test, self.y_test_path)
+        self.config["SPLIT_DATA"] = {'X_train': self.X_train_path,
+                                     'y_train': self.y_train_path,
+                                     'X_test': self.X_test_path,
+                                     'y_test': self.y_test_path}
+
+        self.log.info("X_train, y_train and X_test, y_test data is ready")
+
         with open('config.ini', 'w') as configfile:
             self.config.write(configfile)
-        return os.path.isfile(self.train_path[0]) and\
-            os.path.isfile(self.train_path[1]) and\
-            os.path.isfile(self.test_path[0]) and \
-            os.path.isfile(self.test_path[1])
+        return os.path.isfile(self.X_train_path) and\
+            os.path.isfile(self.y_train_path) and\
+            os.path.isfile(self.X_test_path) and \
+            os.path.isfile(self.y_test_path)
 
     def save_splitted_data(self, df: pd.DataFrame, path: str) -> bool:
         df = df.reset_index(drop=True)
